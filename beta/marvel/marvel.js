9537d500064700c25d1a09b1b88ff7e12356e6f2
TsurukoSama <47467453+TsurukoSama@users.noreply.github.com>
2019-04-13 13:19:57 -0700

    Update marvel.js

--- beta/marvel/marvel.js
+++ beta/marvel/marvel.js
@@ -1,4 +1,14 @@
 function searchMarvelApi(comic){
+    
+    var config = {
+    apiKey: "AIzaSyA8kEgnv75jLgZznRXBSpD7FGHd0ulv8Jo",
+    authDomain: "project-one-7cd8f.firebaseapp.com",
+    databaseURL: "https://project-one-7cd8f.firebaseio.com",
+    projectId: "project-one-7cd8f",
+    storageBucket: "project-one-7cd8f.appspot.com",
+    messagingSenderId: "147656414734"
+  };
+  firebase.initializeApp(config);
 
     var publicKey = '7b343b8fcbca56aa7f9d16d2c4ed1c16'
     var privateKey = '5fb816eff68dc0f6948e192c163fdd3133442044'
